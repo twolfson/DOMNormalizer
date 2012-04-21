b888e7c18e09ffbbf19b658924227cd74253da4d
Todd Wolfson <todd@twolfson.com>
2012-04-20 18:24:48 -0700

    Running into IE7 bug

--- src-test/normalizer.test.js
+++ src-test/normalizer.test.js
@@ -6,25 +6,33 @@ var suite = new Skeleton('DOMNormalizer'),
 suite.addBatch({
   'A text input': {
     topic: function () {
+      return document.getElementById('TESTinput');
     },
     'that has been DOMNormalized': {
-      'can listen to a standard event (onchange)': {
-        'without throwing errors': '',
-        'and unsubscribe to it': {
-          'without throwing errors': ''
-        }
+      topic: function (input) {
+        return new DOMNormalizer(input);
       },
-      'can trigger a standard event (onchange)': {
-        'without throwing errors': ''
+      'can listen to a standard event (onchange) without throwing errors': function ($input) {
+        $input.on('change', function () {});
+        assert(true);
       },
-      'can listen to a non-standard event (onwiggle)': {
-        'without throwing errors': '',
-        'and unsubscribe to it': {
-          'without throwing errors': ''
-        }
+      // 'and unsubscribe to it': {
+        // 'without throwing errors': ''
+      // }
+      'can trigger a standard event (onclick) without throwing errors': function ($input) {
+        $input.trigger('change', function () {});
+        assert(true);
       },
-      'can trigger a non-standard event (onwiggle)': {
-        'without throwing errors': ''
+      'can listen to a non-standard event (onwiggle) without throwing errors': function ($input) {
+        $input.on('wiggle', function () {});
+        assert(true);
+      },
+      // 'and unsubscribe to it': {
+        // 'without throwing errors': ''
+      // }
+      'can trigger a non-standard event (onwobble) without throwing errors': function ($input) {
+        $input.trigger('wiggle', function () {});
+        assert(true);
       }
     }
   }
