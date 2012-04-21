ecedb436b1b9e8c327f2cd7925be94228d90399a
Todd Wolfson <todd@twolfson.com>
2012-04-20 18:16:42 -0700

    Setting up tests

--- src-test/normalizer.test.js
+++ src-test/normalizer.test.js
@@ -4,10 +4,48 @@ var suite = new Skeleton('DOMNormalizer'),
 
 // First batch: Basics
 suite.addBatch({
+  'A text input': {
+    topic: function () {
+    },
+    'that has been DOMNormalized': {
+      'can listen to a standard event (onchange)': {
+        'without throwing errors': '',
+        'and unsubscribe to it': {
+          'without throwing errors': ''
+        }
+      },
+      'can trigger a standard event (onchange)': {
+        'without throwing errors': ''
+      },
+      'can listen to a non-standard event (onwiggle)': {
+        'without throwing errors': '',
+        'and unsubscribe to it': {
+          'without throwing errors': ''
+        }
+      },
+      'can trigger a non-standard event (onwiggle)': {
+        'without throwing errors': ''
+      }
+    }
+  }
 });
 
 // Second batch: Intermediate
 suite.addBatch({
+  'A text input': {
+    'that has been DOMNormalized': {
+      'that is listening for a standard event (onclick)': {
+        'and one occurs': {
+          'then the listener is triggered': ''
+        }
+      },
+      'that is listening for a non-standard event (onswizzle)': {
+        'and one occurs': {
+          'then the listener is triggered': ''
+        }
+      }
+    }
+  }
 });
 
 // Third batch: Advanced
